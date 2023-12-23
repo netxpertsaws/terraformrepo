5a7f313ebd00403b346e044d3eff91e87db58da3
netxpertsaws <98447003+netxpertsaws@users.noreply.github.com>
2023-12-23 12:25:18 +0530

    Update newec2.tf

--- newec2.tf
+++ newec2.tf
@@ -4,8 +4,7 @@ terraform {
     key            = "dev/teraform.tfstate"
     region         = "us-east-1"
     dynamodb_table = "terraform-s-state"
-    access_key     = "AKIA4WG2YRJRXB7TYKIW"
-    secret_key     = "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"
+   
   }
 }
 
